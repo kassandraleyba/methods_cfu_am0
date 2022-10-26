7365eea30a203e57f9edfd195db2826a358821f8
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-10-26 07:27:01 -0600

    Add changes to #2-#4

--- final_practice.rb
+++ final_practice.rb
@@ -26,7 +26,8 @@ puts greet_a_person ("Mandy")
 #The return value of my method is: Hello! Good to see you, Mandy.
 
 # How many arguments did you pass your method?
-#I passed one argument in my method: name.
+# I passed one argument in my method: "Mandy"
+# The paramteter for my method definition is name.
 
 # What data type was your argument(s)?
 #The data type of my argument was a string.
@@ -35,34 +36,35 @@ puts greet_a_person ("Mandy")
 
 
 #3: Write a method named square that takes in one number, and returns the square of that number
-def square
-    4**4
+
+def square(number)
+    number**2
 end
 
-puts square
+puts square(6)
+# puts square(3) example
+# puts square(10) example
+
 
 # What is the return value of your method?
-#The return value of my method is the integer 256.
+# The return value of my method is 36.
 
 # How many arguments did you pass your method?
-#I did not pass any arguments.
+# I passed one argument: 6
 
 # What data type was your argument(s)?
-#The data type of my argument was an integer.
+#The data type of my argument is an integer.
 
 
 
 
 #4: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
-first_name = "Kassandra"
-middle_name = "Paige"
-last_name = "Leyba"
 
 def greet_person(first_name, middle_name, last_name)
     "Hola, #{first_name} #{middle_name} #{last_name}!"
 end
 
-puts greet_person(first_name, middle_name, last_name)
+puts greet_person("Kassandra", "Paige", "Leyba")
 
 
 # What is the return value of your method?
@@ -72,5 +74,4 @@ puts greet_person(first_name, middle_name, last_name)
 #I passed 3 arguments.
 
 # What data type was your argument(s)?
-#The data types were all strings.
-
+#The data types are all strings.
